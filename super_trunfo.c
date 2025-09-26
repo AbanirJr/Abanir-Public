59295920c6b7ed982f42b84a5fe820f2c8dd878e
AbanirJr <abanirjr@gmail.com>
2025-09-26 14:09:01 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,4 +1,98 @@
 # include <stdio.h>
 
 void main()
-{  printf("Apenas inicializando...");  }
+{   
+// DECLARANDO AS VARIÁVEIS
+  char UF1[2];
+  char CIDADE1[20];
+  char CODIGO_CARTA1[3];
+  int HABITANTES1;
+  float AREA1;
+  float PIB1;
+  int PT_TUR1;
+  char UF2[2];
+  char CIDADE2[20];
+  char CODIGO_CARTA2[3];
+  int HABITANTES2;
+  float AREA2;
+  float PIB2;
+  int PT_TUR2;
+  
+// INSERINDO OS DADOS DA CARTA 1
+  printf ("Forneça as Informações das Cidades nas Cartas do SUPER TRUNFO \n");
+  printf ("\n");
+  printf ("CARTA 1");
+  printf ("\n");
+  printf ("Estado: ");
+  scanf (%d,&UF1[]);
+  printf ("\n");
+  printf ("Código da Carta: ");
+  scanf (%d,&CODIGO_CARTA1[]);
+  printf ("\n");  
+  printf ("Nome da Cidade: ");
+  scanf (%d,&CIDADE1[]);
+  printf ("\n");
+  printf ("Número de Habitantes: ");
+  scanf (%d,&HABITANTES1);
+  printf ("\n");
+  printf ("Área da Cidade: ");
+  scanf (%f,&AREA1);
+  printf ("\n");
+  printf ("PIB: ");
+  scanf (%f,&PIB1);
+  printf ("\n");
+  printf ("Número de Pontos Turísticos: ");
+  scanf (%d,&PT_TUR1);
+  printf ("\n");
+
+// INSERINDO OS DADOS DA CARTA 2
+  printf ("Forneça as Informações das Cidades nas Cartas do SUPER TRUNFO \n");
+  printf ("\n");
+  printf ("CARTA 2");
+  printf ("\n");
+  printf ("Estado: ");
+  scanf (%d,&UF2[]);
+  printf ("\n");
+  printf ("Código da Carta: ");
+  scanf (%d,&CODIGO_CARTA2[]);
+  printf ("\n");  
+  printf ("Nome da Cidade: ");
+  scanf (%d,&CIDADE2[]);
+  printf ("\n");
+  printf ("Número de Habitantes: ");
+  scanf (%d,&HABITANTES2);
+  printf ("\n");
+  printf ("Área da Cidade: ");
+  scanf (%f,&AREA2);
+  printf ("\n");
+  printf ("PIB: ");
+  scanf (%f,&PIB2);
+  printf ("\n");
+  printf ("Número de Pontos Turísticos: ");
+  scanf (%d,&PT_TUR2);
+  printf ("\n");
+
+// IMPRIMINDO AS DUAS CARTAS
+  printf (" ____________________________________________________________________________________________________ \n");
+  printf (f"| CARTA ",&CODIGO_CARTA1[],"                                        |  | CARTA ",&CODIGO_CARTA2[],"                                        | \n");
+  printf (f"|  Estado: ",&UF1[],"                          | | Estado: ",&UF2[],"                          | \n");
+  printf (f"| Município: ",&CIDADE1[],"                  |  | Município: ",&CIDADE2[],"                  | \n");
+  printf (f"População: ",&HABITANTES1,"                 |  | População: ",&HABITANTES2,"            | \n");
+  printf (f"Área do Município: ",&AREA1,"               |  | Área do Município: ",&AREA2,"          | \n");
+  printf (f"Produto Interno Bruto: ",&PIB1,"            |  | Produto Interno Bruto: ",&PIB2,"       | \n");
+  printf (f"Pontos Turísticos: ",&PT_TUR1,"              |  | Pontos Turísticos: ",&PT_TUR2,"        | \n");
+  printf ("|___________________________________________________________________________________________________| \n");
+}
+
+  
+      
+  
+  
+  
+
+  
+  
+  
+  
+  
+  printf("Apenas inicializando...");  }
