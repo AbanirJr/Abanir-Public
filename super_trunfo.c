2db87fc23ab6525bd363d87fdb8cd869731fcd64
AbanirJr <abanirjr@gmail.com>
2025-09-27 00:24:18 +0000

    Esboço completo 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -24,13 +24,13 @@ void main()
   printf ("CARTA 1");
   printf ("\n");
   printf ("Estado: ");
-  scanf ("%s",&UF1[]);
+  scanf ("%s",&UF1);
   printf ("\n");
   printf ("Código da Carta: ");
-  scanf ("%s",&CODIGO_CARTA1[]);
+  scanf ("%s",&CODIGO_CARTA1);
   printf ("\n");  
   printf ("Nome da Cidade: ");
-  scanf ("%s",&CIDADE1[]);
+  scanf ("%s",&CIDADE1);
   printf ("\n");
   printf ("Número de Habitantes: ");
   scanf ("%d",&HABITANTES1);
@@ -51,13 +51,13 @@ void main()
   printf ("CARTA 2");
   printf ("\n");
   printf ("Estado: ");
-  scanf ("%s",&UF2[]);
+  scanf ("%s",&UF2);
   printf ("\n");
   printf ("Código da Carta: ");
-  scanf ("%s",&CODIGO_CARTA2[]);
+  scanf ("%s",&CODIGO_CARTA2);
   printf ("\n");  
   printf ("Nome da Cidade: ");
-  scanf ("%s",&CIDADE2[]);
+  scanf ("%s",&CIDADE2);
   printf ("\n");
   printf ("Número de Habitantes: ");
   scanf ("%d",&HABITANTES2);
@@ -74,9 +74,9 @@ void main()
 
 // IMPRIMINDO AS DUAS CARTAS
   printf (" ____________________________________________________________________________________________________ \n");
-  printf ("| CARTA %s",&CODIGO_CARTA1[],"                                        |  | CARTA %s",&CODIGO_CARTA2[],"                                        | \n");
-  printf ("|  Estado: %s",&UF1[],"                          | | Estado: %s",&UF2[],"                          | \n");
-  printf ("| Município: %s",&CIDADE1[],"                  |  | Município: %s",&CIDADE2[],"                  | \n");
+  printf ("| CARTA %s",&CODIGO_CARTA1,"                                        |  | CARTA %s",&CODIGO_CARTA2,"                                        | \n");
+  printf ("|  Estado: %s",&UF1,"                          | | Estado: %s",&UF2,"                          | \n");
+  printf ("| Município: %s",&CIDADE1,"                  |  | Município: %s",&CIDADE2,"                  | \n");
   printf ("População: %d",&HABITANTES1,"                 |  | População: %d",&HABITANTES2,"            | \n");
   printf ("Área do Município: %.2f",&AREA1,"               |  | Área do Município: %.2f",&AREA2,"          | \n");
   printf ("Produto Interno Bruto: %.2f",&PIB1,"            |  | Produto Interno Bruto: %.2f",&PIB2,"       | \n");
